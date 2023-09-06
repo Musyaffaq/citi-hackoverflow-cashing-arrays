5382b553e722445fe7ce4acae32664f403c22824
ilovedangomilk <123913697+ilovedangomilk@users.noreply.github.com>
2023-09-07 03:20:51 +0800

    Update pymongo_insert.py
    Included function to combine newsarticles with stockprice

--- backend/pymongo_insert.py
+++ backend/pymongo_insert.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 
-def insert_to_db(data): 
-
+def insert_to_db(news_articles, stockprice): 
+    for i in range(len(news_articles)):
+        news_articles['stockprice']: stockprice[i] 
+    data = news_articles
     CONNECTION_STRING = "mongodb+srv://edwardsim2021:6wRN4koGWqLyHhqW@tesla-news-dates-and-ta.zrbnzax.mongodb.net/"
     client = MongoClient (CONNECTION_STRING)
     dbname = client['tesla-news']
